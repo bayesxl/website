03883a01c71e144fc27d6254197c8fb98c3d4c24
Michael Cohen <mkc1000@gmail.com>
2020-05-21 11:35:21 +0100

    Add TODO

--- automate_publications.py
+++ automate_publications.py
@@ -17,7 +17,7 @@ def copytree(src, dst, symlinks=False, ignore=None):
             if not os.path.exists(d) or os.stat(s).st_mtime - os.stat(d).st_mtime > 1:
                 shutil.copy2(s, d)
 
-
+#TODO: change to appropriate directory
 os.chdir('/Users/mosb/Documents/WWW/MosbWWW/robots-www/')
 
 
